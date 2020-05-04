13c583b20206d92c38299d280be64c026acc6a8b
paikwiki <paikwiki@gmail.com>
2020-05-04 11:22:12 +0900

    Create a new project: Quiz

---
+++ codes/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Changhyun Baek on 2020/05/04.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
