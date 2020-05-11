e9620086a292ff79a5e15b2423dfff5e25b02799
paikwiki <paikwiki@gmail.com>
2020-05-11 22:45:08 +0900

    Write a note(Ch02) and done Chapter 02

--- ch02-the-swift-language.md
+++ ch02-the-swift-language.md
@@ -69,18 +69,76 @@ class MyClass: SuperClass {
 
 - 문자열 리터럴: 큰따옴표 사용
 - 숫자 리터럴
-- 서브스크립팅: 배열에 접근할 수 있는 단축어
+- 서브스크립팅: 배열에 접근할 수 있는 단축어. 딕셔너리에도 사용할 수 있음
 - 배열 밖의 인덱스(an out-of-bounds index) 참조시 trap 발생
 - trap: 알 수 없는 상태에 들어가기 전에 프로그램을 중단시키는 런타임 에러
 
-이니셜라이져
+이니셜라이져(Initializers)
+
+- 인스턴스(instance): 자료형의 특정 형태(a particular embodiment of a type). 역사적으로 인스턴스라는 용어는 클래스와 함께 쓰였으나 스위프트는 스트럭쳐나 이뉴머레이션에도 사용함
+- 이니셜라이저의 동작이 끝나면 인스턴스는 동작(action)할 준비 완료
+- 실수에 대해 타입추론을 할 경우 기본 자료형은 더블
+
+속성(Properties)
+
+- 속성: 자료형의 인스턴스와 관련한 값
+
+인스턴스 메서드(Instance methods)
+
+- 인스턴스 메서드: 특정 자료형에 한정된 함수로 해당 자료형의 인스턴스에서 호출할 수 있음
 
 ## 옵셔널
 
-## Loops and String Interpolation
+- 자료형 뒤에 `?`를 붙여서 옵셔널을 지정할 수 있다.
+- 변수가 어떤 값도 저장하지 않을 수 있는 가능성이 있음을 나타냄
+- 옵셔널은 지정한 자료형의 인스턴스이거나 nil 일수 있음
+- 옵셔널로 지정한 변수는 반드시 언래핑해서 사용해야 함(변수명 뒤에 `!`를 붙임)
 
-## Enumerations and the Switch Statement
+옵셔널 바인딩
 
-## 클로저
+- 옵셔널로 지정한 변수에 값이 없는데 언래핑을 시도할 경우 nil이 반환됨. 이는 trap의 원인이 됨.
+- 트랩을 방지하기 위해 옵셔널 바인이(optional binding) 활용
 
-## 애플 스위프트 공식 문서 찾아보기
+딕셔너리 서브스크립팅
+
+배열에서 out-of-bounds 인덱스를 사용하면 트랩이 발생하는 것과 달리 딕셔너리는 옵셔널을 반환함
+
+## 반목문과 문자열 보간(Loops and String Interpolation)
+
+````swift
+for (i, string) in countingUp.enumerated() {
+    print(i, string)
+}
+````
+
+위의 코드에서 `enumerated()`는 튜플(tuples)을 반환한다.
+
+- 튜플: 배열과 유사한, 순서가 있는 값의 그룹
+- 배열과 비교했을 때, 각 멤버가 구별되는 자료형을 갖고 있다는 차별점이 있음
+
+문자열 보간
+
+- 문자열(`""`) 내에서 `\(변수)` 형태로 문자열에 변수의 값을 넣을 수 있음
+
+## 열거형과 스위치 구문(Enumerations and the Switch Statement)
+
+- 열거형: 종류가 각기 다른 값으로 구성된 세트 형태의 자료형(a type with a discrete set of values)
+- 스위치: 스위치 구문의 케이스 구문은 철저해야 한다. 스위치 구문에서 발생할 수 있는 모든 값은 케이스에 포함돼야 하며, 그렇지 않으면 default 케이스를 작성해야 함
+- 스위치 구문은 해당하는 케이스 이후의 구문을 실행하지 않는다(not fall through).
+- fallthrough 키워드로 이후 구문을 실행할 수 있음
+- 열거형은 반환 자료형을 지정하면 rawValue를 지정할 수 있음
+- rawValue가 정수형일 때, 하나의 케이스에 rawValue를 지정하면 다른 케이스에는 저절로 rawValue가 값의 순서대로 대입됨
+
+## 클로저(Closures)
+
+- 기능을 자체 내장한 블록(a self-contained block of functionality)
+- 클로저와 함수들의 차이점: 클로저는 더 컴팩트하고 가벼운 문법을 갖고 있음
+- 이름이나 완전한 함수 선언 없이 "함수-같은" 구조를 사용할 수 있음
+
+예시
+
+````swift
+let compareAscending = { (i: Int, j: Int) -> Bool in
+    return i < j
+}
+````
--- codes/ch02-playground/MyPlayground.playground/Contents.swift
+++ codes/ch02-playground/MyPlayground.playground/Contents.swift
@@ -1,9 +1,14 @@
 import UIKit
 
+// 2.2. Using Standard Types
+// =========================
+
 var str = "Hello, playground"
 str = "Hello, Swift"
 let constStr = str
 //constStr = "Hello, world"
+
+// 2.2.2. Specifying types
 
 var nextYear: Int = 0
 var bodyTemp: Float = 0
@@ -14,8 +19,127 @@ var arrayOfStrings: [String] = []
 var dictionaryOfCapitalsByCountry: [String: String] = [:]
 var winningLotteryNumbers: Set<Int> = []
 
+// 2.2.3. Literals and subscripting
+
 let number = 42
 let fmStatiion = 91.2
 
-let countingUp = ["one", "two"]
+//let countingUp = ["one", "two"]
+var countingUp = ["one", "two"] // change for 2.2.6. Instance methods
 let secondElement = countingUp[1]
+
+// 2.2.4. Initializers
+
+let defaultNumber = Int()
+let defaultBool = Bool()
+let meanigOfLife = String(number)
+let availableRooms = Set([205, 411, 412])
+
+let defaultFloat = Float()
+let floatFromLiteral = Float(3.14)
+
+let easyPi = 3.14
+let floatFromDouble = Float(easyPi)
+let floatingPi: Float = 3.14
+
+// 2.2.5. Properties
+
+countingUp.count
+let emptyString = String()
+emptyString.isEmpty
+
+// 2.2.6. Instance methods
+
+countingUp.append("three")
+
+// 2.3. Optionals
+// ==============
+
+var anOptionalFloat: Float?
+var anOptionalArrayOfStrings: [String]?
+var anOptionalArrayOfOptionalStrings: [String?]?
+
+var reading1: Float?
+var reading2: Float?
+var reading3: Float?
+
+reading1 = 9.8
+reading2 = 9.2
+//reading3 = 9.7
+
+//let avgReading = (reading1 + reading2 + reading3) / 3 // Error
+//let avgReading = (reading1! + reading2! + reading3!) / 3
+if let r1 = reading1,
+    let r2 = reading2,
+    let r3 = reading3 {
+        let avgReading = (r1 + r2 + r3) / 3
+        print(avgReading)
+} else {
+    let errorString = "Instrument reported a reading that was nil."
+    print(errorString)
+}
+
+// 2.3.1. Subsriptiong dictionaries
+
+let nameByParkingSpace = [13: "Alice", 27: "Bob"]
+//let space13Assignee: String? = nameByParkingSpace[13]
+if let space13assignee = nameByParkingSpace[13] {
+    print(space13assignee)
+}
+let space42Assignee: String? = nameByParkingSpace[42]
+
+// 2.4. Loops and String Interpolation
+
+let range = 0..<countingUp.count
+for i in range {
+    let string = countingUp[i]
+    // Use 'string'
+    print(string)
+}
+
+for (i, string) in countingUp.enumerated() {
+    print(i, string)
+}
+
+for (space, name) in nameByParkingSpace {
+    let permit = "Space \(space): \(name)"
+    print(permit)
+}
+
+// 2.5. Enumeratoins and the Switch Statement
+
+enum PieType: Int {
+    case apple = 0
+    case cherry
+    case pecan
+}
+let favoritePie = PieType.apple
+let name: String
+switch favoritePie {
+case .apple:
+    name = "Apple"
+case .cherry:
+    name = "Cherry"
+case .pecan:
+    name = "Pecan"
+}
+
+let pieRwaValue = PieType.pecan.rawValue
+
+if let pieType = PieType(rawValue: pieRwaValue) {
+    // Got a valid 'pieType'!
+    print(pieType)
+}
+// 2.6. Closures
+
+let compareAscending = { (i: Int, j: Int) -> Bool in
+    return i < j
+}
+compareAscending(42, 2)
+compareAscending(-2, 12)
+
+var numbers = [42, 9, 12, -17]
+numbers.sort(by: compareAscending)
+numbers.sort(by: { (i: Int, j: Int) -> Bool in
+    return i < j
+})
