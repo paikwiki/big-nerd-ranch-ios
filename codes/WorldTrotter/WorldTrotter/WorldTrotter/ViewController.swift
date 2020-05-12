55a7481a2887ea4db57cb5615428b7336b8db08a
paikwiki <paikwiki@gmail.com>
2020-05-12 16:08:47 +0900

    notes: Create a note | codes: Create new project
    - notes: Chapter 3.
    - codes: WorldTrotter
    - This commit should be seperated by 2 commits.

---
+++ codes/WorldTrotter/WorldTrotter/WorldTrotter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Changhyun Baek on 2020/05/12.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
