9457371f28b5a509c72560594809183e9505e4ec
paikwiki <paikwiki@gmail.com>
2020-05-13 16:02:03 +0900

    [WorldTrotter] Done Chapter04

---
+++ codes/WorldTrotter/WorldTrotter/WorldTrotter/MapViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Changhyun Baek on 2020/05/13.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("MapViewController loaded its view.")
+    }
+}
