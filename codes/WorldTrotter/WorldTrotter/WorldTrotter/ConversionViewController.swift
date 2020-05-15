ddb6b8a53101591aa50e65c447c6e4bf8deec2b7
paikwiki <paikwiki@gmail.com>
2020-05-15 18:03:37 +0900

    [WorldTrotter] Done "6.1. Text Editing" in Chapter06(WIP)

--- codes/WorldTrotter/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ codes/WorldTrotter/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -9,10 +9,51 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = "\(celsiusValue.value)"
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("ConversionViewController loaded its view.")
+        
+        updateCelsiusLabel()
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
     }
 }
 
