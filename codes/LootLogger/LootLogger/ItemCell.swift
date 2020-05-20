9e73415b71d3c496e9713fe97b4b787d26e2aaa0
paikwiki <paikwiki@gmail.com>
2020-05-20 10:52:52 +0900

    [LootLogger] Done Chpater10

---
+++ codes/LootLogger/LootLogger/ItemCell.swift
@@ -0,0 +1,17 @@
+//
+//  ItemCell.swift
+//  LootLogger
+//
+//  Created by Changhyun Baek on 2020/05/20.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+    
+}
--- codes/LootLogger/LootLogger/ItemsViewController.swift
+++ codes/LootLogger/LootLogger/ItemsViewController.swift
@@ -70,22 +70,25 @@ class ItemsViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         // Get a new or recycled cell
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will apper in on the table view
         let item = itemStore.allItems[indexPath.row]
         
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        cell.nameLabel.text = item.name
+        cell.serialNumberLabel.text = item.serialNumber
+        cell.valueLabel.text = "$\(item.valueInDollars)"
         
         return cell
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 65
     }
     
 }
