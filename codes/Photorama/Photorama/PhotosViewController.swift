43d046c70150d572b4d5d24630a01abf3ca06af7
paikwiki <paikwiki@gmail.com>
2020-05-29 10:53:59 +0900

    [Photorama] Create a project: Photorama

---
+++ codes/Photorama/Photorama/PhotosViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Changhyun Baek on 2020/05/29.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
