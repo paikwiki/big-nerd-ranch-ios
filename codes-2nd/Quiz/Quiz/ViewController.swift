9ba15834ed9adeed4173abf83cdff1488cef6fb3
paikwiki <paikwiki@gmail.com>
2020-06-25 17:31:32 +0900

    [Quiz] Done Quiz project again

---
+++ codes-2nd/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Changhyun Baek on 2020/06/25.
+//  Copyright © 2020 paikwiki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Graptes"
+    ]
+    var currentQuestionIndex: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+}
